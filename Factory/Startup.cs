7c86f99fa77d454f328b919f7260c72459ef7dbd
Nick Tse <nicholashtse@gmail.com>
2022-08-15 12:16:03 -0700

    add index view for engineers

--- Factory/Startup.cs
+++ Factory/Startup.cs
@@ -43,7 +43,7 @@ namespace Factory
       
       app.Run(async (context) =>
       {
-        await context.Response.WriteAsync("Hello World!");
+        await context.Response.WriteAsync("Default page");
       });
     }
   }
